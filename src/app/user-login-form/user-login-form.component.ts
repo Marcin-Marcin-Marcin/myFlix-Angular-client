15878ed1f448fa7702194ec3bc67f4413246d89a
MarcinBiesiada <mtbiesiada@gmail.com>
2025-09-08 14:48:56 +0200

    registration and login dialgos
    User registration and login components, call API, store token/user, update app.module

---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.scss']
+})
+export class UserLoginFormComponent {
+  credentials = { Username: '', Password: '' };
+
+  constructor(
+    public api: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar
+  ) {}
+
+  login(): void {
+    this.api.userLogin(this.credentials).subscribe(
+      (res: any) => {
+        // Expecting { user: {...}, token: '...' }
+        localStorage.setItem('user', JSON.stringify(res.user));
+        localStorage.setItem('token', res.token);
+        this.dialogRef.close();
+        this.snackBar.open('Logged in!', 'OK', { duration: 2000 });
+      },
+      (err) => {
+        this.snackBar.open(err?.error || 'Login failed', 'OK', { duration: 3000 });
+      }
+    );
+  }
+}
